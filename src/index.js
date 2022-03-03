a8f1ec6e7748dd2bae9d3d2e5b749744d39972f9
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-03 14:29:48 +0900

    1일차 수업내용

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>title</h1>
+    <p>paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
